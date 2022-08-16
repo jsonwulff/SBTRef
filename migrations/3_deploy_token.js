0ebbe2b005f1cb7e7a2dd6517416395cae57963c
Christian Påbøl Jacobsen <cpcp800@auroragames.dk>
2022-08-16 12:48:41 +0200

    migrations simpler

--- migrations/3_deploy_token.js
+++ migrations/3_deploy_token.js
@@ -1,13 +1,14 @@
 // Import the HelloWorld contract...
 const Token = artifacts.require("TCGTok");
 
-module.exports = (deployer, network, accounts) => {
+module.exports = async (deployer, network, accounts) => {
   // Deploy it!
-  deployer.deploy(Token).then(()=>{
-    Token.deployed().then(ins=>{
-      let ps = accounts.map(acc => {
-        ins.safeMint(acc, "");
-      });
+  // Instance of token
+  await deployer.deploy(Token)
+  let ins = await Token.deployed()
+  // List of promises of minting
+  let ps = accounts.map(acc => {
+      ins.safeMint(acc, "");
     });
-  });
+  await Promise.all(ps)
 }
--- migrations/4_deploy_registry.js
+++ migrations/4_deploy_registry.js
@@ -6,7 +6,6 @@ const Reg = artifacts.require("TCGReg");
 module.exports = async (deployer, network, accounts) => {
   // Deploy it!
     const ins = await Token.deployed()
-    console.log(ins.address)
     await deployer.deploy(Reg, ins.address)
 
 }
